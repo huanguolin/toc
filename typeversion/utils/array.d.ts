9b2dd67e98f4e396a0e2d3f16d7282b73e201972
alvin huang <alvin.huang@grapecity.com>
2022-08-30 11:08:45 +0900

    add Length<T> and improve Drop<T, N>

--- typeversion/utils/array.d.ts
+++ typeversion/utils/array.d.ts
@@ -1,16 +1,19 @@
+export type Length<T extends any[]> = T['length'];
+
+type tLen = Length<[1, 23]>;
+
 export type Push<A extends any[], E> = [...A, E];
 
-type DropOne<A extends any[]> = A extends [any, ...infer R] ? R : never;
-
-export type Drop<A extends any[], N extends number = 1, AC extends any[] = []> =
-    N extends AC['length']
-        ? A
-        : Drop<DropOne<A>, N, Push<AC, 0>>;
+export type Drop<A extends any[], N extends number = 1> =
+    A extends [...(infer R extends any[]), ...Init<N>]
+        ? R
+        : never;
 
 type t = Drop<[1, 2, 3], 4>;
 
 export type Concat<A1 extends any[], A2 extends any[]> = [...A1, ...A2];
 
-export type Init<L extends number, E = 0, A extends any[] = []> = A['length'] extends L
-    ? A
-    : Init<L, E, Push<A, E>>;
+export type Init<L extends number, E extends any = any, A extends any[] = []> =
+    Length<A> extends L
+        ? A
+        : Init<L, E, Push<A, E>>;
--- typeversion/utils/math.d.ts
+++ typeversion/utils/math.d.ts
@@ -1,20 +1,25 @@
-import { Concat, Drop, Init, Push } from "./array";
+/**
+ * ref: https://itnext.io/implementing-arithmetic-within-typescripts-type-system-a1ef140a6f6f
+ */
 
-export type Plus<N1 extends number, N2 extends number> = Concat<Init<N1>, Init<N2>>['length'];
+import { Concat, Drop, Init, Length, Push } from "./array";
 
-export type Minus<N1 extends number, N2 extends number, A1 extends any[] = Init<N1>, A2 extends any[] = []> =
-    A2['length'] extends N2
-        ? A1['length']
-        : Minus<N1, N2, Drop<A1>, Push<A2, 0>>;
+
+export type Add<N1 extends number, N2 extends number> = Length<Concat<Init<N1>, Init<N2>>>;
+
+export type Sub<N1 extends number, N2 extends number, A1 extends any[] = Init<N1>, A2 extends any[] = []> =
+    Length<A2> extends N2
+        ? Length<A1>
+        : Sub<N1, N2, Drop<A1>, Push<A2, any>>;
 
 export type Mul<N1 extends number, N2 extends number, A1 extends any[] = [], A2 extends any[] = []> =
     N1 extends 0
         ? 0
         : N2 extends 0
             ? 0
-            : A1['length'] extends N1
-                ? A2['length']
-                : Mul<N1, N2, Push<A1, 0>, Concat<A2, Init<N2>>>;
+            : Length<A1> extends N1
+                ? Length<A2>
+                : Mul<N1, N2, Push<A1, any>, Concat<A2, Init<N2>>>;
 
 /**
  * 类似 C 语言的整数除法：
@@ -27,9 +32,9 @@ export type Div<N1 extends number, N2 extends number, A1 extends any[] = Init<N1
         : N1 extends 0
             ? 0
             : A1 extends never
-                ? Drop<A2>['length']
-                : A1['length'] extends 0
-                    ? A2['length']
-                    : Div<N1, N2, Drop<A1, N2>, Push<A2, 0>>;
+                ? Length<Drop<A2>>
+                : Length<A1> extends 0
+                    ? Length<A2>
+                    : Div<N1, N2, Drop<A1, N2>, Push<A2, any>>;
 
-type tDiv = Div<3, 1>;
+type tDiv = Div<9, 4>;
