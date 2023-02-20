811c5b15038ba3738dc3e5b4890e5bb6edcf02c4
alvin huang <alvin.huang@grapecity.com>
2023-02-20 12:01:53 +0900

    fix ts play url not display

--- tools/prepare-ts-playground/toc.js
+++ tools/prepare-ts-playground/toc.js
@@ -8,44 +8,73 @@ const { log, error } = utils.getLogger('toc');
 
 const mergeFilePath = path.resolve('./toc.d.ts');
 
-// Merge to ./toc.d.ts
-try {
-    dts.bundle({
-        name: 'toc',
-        main: '../../type-toc/index.d.ts',
-        out: mergeFilePath,
-        headerPath: path.resolve('./header.txt'),
-    });
-    log('Merge files success!');
-} catch (err) {
-    error(`Merge files error: ${err}`);
-    return;
-}
+// entry point
+main();
 
-// Build url for ts playground.
-let url = '';
-try {
-    const srcCode = fs.readFileSync(mergeFilePath, 'utf8');
-    url = utils.buildTsPlaygroundUrl(srcCode);
-    log('Build url success!');
-} catch (err) {
-    error(`Read file form ${mergeFilePath} error: ${err}`);
-    return;
-}
-
-// Update README.md.
-const readmePath = path.resolve('../../README.md');
-try {
-    let readme = fs.readFileSync(readmePath, 'utf8');
-    readme = readme.replace(/Click\s\[here\]\([^\)]*?\)/, `Click [here](${url}) `);
-    try {
-        fs.writeFileSync(readmePath, readme);
-        log('Update readme success!');
-    } catch (err) {
-        error(`Write file to ${readmePath} error: ${err}`);
+async function main() {
+    // Merge to ./toc.d.ts
+    if (!mergeTypeToc()) {
         return;
     }
-} catch (err) {
-    error(`Read file form ${readmePath} error: ${err}`);
-    return;
+
+    // Build url for ts playground.
+    const url = await buildTsPlayUrl();
+    if (!url) {
+        return;
+    }
+
+    // Update README.md.
+    updateReadme(url);
+}
+
+function mergeTypeToc() {
+    try {
+        dts.bundle({
+            name: 'toc',
+            main: '../../type-toc/index.d.ts',
+            out: mergeFilePath,
+            headerPath: path.resolve('./header.txt'),
+        });
+        log('Merge files success!');
+    } catch (err) {
+        error(`Merge files error: ${err}`);
+        return false;
+    }
+    return true;
+}
+
+async function buildTsPlayUrl() {
+    let url = '';
+    try {
+        const srcCode = fs.readFileSync(mergeFilePath, 'utf8');
+        try {
+            url = await utils.buildTsPlaygroundUrl(srcCode);
+            log('Build url success!');
+        } catch (err) {
+            error(`Build ts play ground url error: ${err}`);
+        }
+    } catch (err) {
+        error(`Read file form ${mergeFilePath} error: ${err}`);
+    }
+    return url;
+}
+
+function updateReadme(url) {
+    const readmePath = path.resolve('../../README.md');
+    try {
+        let readme = fs.readFileSync(readmePath, 'utf8');
+        readme = readme.replace(/Click\s\[here\]\([^\)]*?\)/, `Click [here](${url})`);
+        try {
+            fs.writeFileSync(readmePath, readme);
+            log('Update readme success!');
+        } catch (err) {
+            error(`Write file to ${readmePath} error: ${err}`);
+            return false;
+        }
+    } catch (err) {
+        error(`Read file form ${readmePath} error: ${err}`);
+        return false;
+    }
+
+    return true;
 }
--- tools/prepare-ts-playground/utils.js
+++ tools/prepare-ts-playground/utils.js
@@ -1,14 +1,34 @@
 const LZString = require('lz-string');
+const axios = require('axios');
 
 module.exports = {
     getLogger,
     buildTsPlaygroundUrl,
 };
 
-function buildTsPlaygroundUrl(srcCode) {
+async function buildTsPlaygroundUrl(srcCode) {
     const urlPrefix = 'https://www.typescriptlang.org/play?#code/';
     const encodedSrcCode = LZString.compressToEncodedURIComponent(srcCode);
-    return urlPrefix + encodedSrcCode;
+    const longUrl = urlPrefix + encodedSrcCode;
+    return await shortenTsPlayUrl(longUrl);
+}
+
+async function shortenTsPlayUrl(longUrl) {
+    // Thanks to https://tsplay-dev.vercel.app/
+    const body = {
+        createdOn: 'client',
+        expires: false,
+        url: longUrl,
+    };
+    const res = await axios.post('https://tsplay.dev/api/short', body, {
+        headers: {
+            ['content-type']: 'application/json',
+            ['origin']: 'https://tsplay-dev.vercel.app',
+            ['referer']: 'https://tsplay-dev.vercel.app/',
+            ['user-agent']: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36',
+        },
+    });
+    return res.data.shortened;
 }
 
 function getLogger(name) {
